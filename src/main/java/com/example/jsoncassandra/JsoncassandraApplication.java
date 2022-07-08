c8bedb794e893d2b6742aa673b29d15b42a7aee1
Phuoc Ngo <cuibaps1@users.noreply.github.com>
2022-07-08 09:49:46 +0700

    first commit

---
+++ src/main/java/com/example/jsoncassandra/JsoncassandraApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsoncassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoncassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsoncassandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jsoncassandra/JsoncassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsoncassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsoncassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
